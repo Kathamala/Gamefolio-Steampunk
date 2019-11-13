14a5f0638ab18e3d59ca9035bba6b4aed8c9360b
Thiago Vinícius <thiagovao2000@hotmail.com>
2019-11-13 15:09:50 -0300

    Gerar cenas aleatorias
    Gerar cenas aleatorias sem repetição

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+
+    static public List<int> listaFases = new List<int>(){0, 0, 0, 0, 0};
+    static public int faseAtual = 0;
+    int fase, i = 0, j = 0;
+    bool verificador = true;
+
+    void Start()
+    {
+        while(i < 5)
+        {
+            while (listaFases[i] == 0)
+            {
+                fase = Random.Range(1, 6);
+                verificador = true;
+                for (j=0;j<5;j++)
+                {
+                    if (listaFases[j] == fase)
+                    {
+                        verificador = false;
+                        j = 5;
+                    }
+                }
+                if (verificador == true)
+                {
+                    listaFases[i] = fase;
+                }
+            }
+            i++;
+        }
+
+        for (j = 0; j < 5; j++)
+        {
+            Debug.Log(listaFases[j]);
+        }
+    }
+}
---
+++ Assets/Scripts/Scenes.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Scenes : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("Clicou");
+            SceneManager.LoadScene(MenuScript.listaFases[MenuScript.faseAtual]);
+            MenuScript.faseAtual++;
+        }
+    }
+}
